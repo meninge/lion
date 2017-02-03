fd9ea08bbf649756510e9f750af0ce0e2dbb7329
Hugues de Valon <hugues.de-valon@phelma.grenoble-inp.fr>
2017-02-03 11:39:00 +0100

    Prepare project for the demo

--- dataset.h
+++ dataset.h
@@ -2,53 +2,13 @@
 #define _DATASET_H_
 
 /*
+ * DEMO VERSION : only use 10 digits in frame.c
+ *
  * Comment this next line to use the data below instead of official MNIST frames
  * If you choose to use MNIST, you will have to import into the project the
  * MNIST data arrays frames and labels and give weights.
  * Make sure that the configuration matches with the hardware.
  */
-#define MNIST
-
-#ifndef MNIST
-
-/*
- * User defined configuration
- * If you change FSIZE, NEU1 and NEU2, they have to be changed in the hardware
- * aswell.
- */
-#define FRAMES_NB 2
-#define FSIZE     64
-#define NEU1      4
-#define NEU2      3
-
-// Weights for neurons layer 1
-const int16_t config_neu1[NEU1][FSIZE] = {
-	{1, -50, 10541, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -20},
-	{2, 50, -1152, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -444},
-	{3, 514, 453, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 666},
-	{4, 1248, 14000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 777}
-};
-
-// Constants for recode after layer 1
-const int16_t config_recode1[NEU1] = {1, 2, 3, 4};
-
-// Weights for neurons layer 2
-const int16_t config_neu2[NEU2][NEU1] = {
-	{ 3, 1, 1, 1},
-	{ 2, 1, 1, 1},
-	{ 1, 1, 1, 1}
-};
-
-// Constants for recode after layer 2
-const int16_t config_recode2[NEU2] = {0, 0, 0};
-
-// Frames to send
-const uint8_t data_frames[FRAMES_NB][FSIZE] = {
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
-
-#else
 
 /*
  * MNIST data
@@ -74,7 +34,5 @@ extern int16_t b2[NEU2];
  */
 extern uint8_t frames[FRAMES_NB][FSIZE];
 extern uint8_t labels[FRAMES_NB];
-
-#endif
 
 #endif /* _DATASET_H_ */
--- main.c
+++ main.c
@@ -23,26 +23,27 @@
 #include "accregs.h"
 #include "demo.h"
 
+/*
+ * main.c
+ * demo version: use 10 MNIST frames and print the guessed digits.
+ */
 int main()
 {
-	int image, neuron;
-	int difference_soft_hard = 0;
-	uint32_t results_soft[FRAMES_NB][NEU2];
+	int image;
 	uint32_t results_hard[FRAMES_NB][NEU2];
-	uint32_t classification_soft[FRAMES_NB];
 	uint32_t classification_hard[FRAMES_NB];
 	uint32_t success_hits_hard = 0;
-	uint32_t success_hits_soft = 0;
 
 	init_platform();
 
 	print("     --------------------------\n");
 	print("     | MENINGE NEURAL NETWORK |\n");
+	print("     | DEMO VERSION           |\n");
 	print("     | Paul Luperini          |\n");
 	print("     | Lucas Mahieu           |\n");
 	print("     | Hugues de Valon        |\n");
 	print("     --------------------------\n\n");
-	sleep(3);
+	sleep(8);
 
 	// Explicitely enable Rx ad Tx
 	XUartPs_WriteReg(XPAR_PS7_UART_1_BASEADDR, XUARTPS_CR_OFFSET,
@@ -54,95 +55,27 @@ int main()
 	XUartPs_WriteReg(XPAR_PS7_UART_1_BASEADDR, XUARTPS_IDR_OFFSET,
 			XUARTPS_IXR_MASK);
 
-#ifdef MNIST
-	/*
-	 * On passe à la fonction un tableau à deux dimensions en lui donnant
-	 * l'adresse de la première case du tableau.
-	 */
-	printf("hardware computation... ");
-	fflush(stdout);
 	nn_hardware(&frames[0][0], &results_hard[0][0],	&w1[0][0], &w2[0][0],
 			b1, b2);
-	printf("done.\n");
-	printf("software computation... ");
-	fflush(stdout);
-	nn_software(&frames[0][0], &results_soft[0][0],	&w1[0][0], &w2[0][0],
-			b1, b2);
-	printf("done.\n");
-#else
-	printf("hardware computation... ");
-	fflush(stdout);
-	nn_hardware(&data_frames[0][0], &results_hard[0][0],
-			&config_neu1[0][0], &config_neu2[0][0],
-			config_recode1, config_recode2);
-	printf("done.\n");
-	printf("software computation... ");
-	fflush(stdout);
-	nn_software(&data_frames[0][0], &results_soft[0][0],
-			&config_neu1[0][0], &config_neu2[0][0],
-			config_recode1, config_recode2);
-	printf("done.\n");
-#endif /* MNIST */
-
-	/*
-	 * Compare hard and soft results.
-	 */
-	for (image = 0; image < FRAMES_NB; image++) {
-		for (neuron = 0; neuron < NEU2; neuron++) {
-			if (results_soft[image][neuron] !=
-					results_hard[image][neuron]) {
-				difference_soft_hard = 1;
-				printf("WARNING: difference soft/hard\n");
-				printf("soft: image n°%3d, neurone n°%3d : %10lu\n",
-						image, neuron,
-						(unsigned long)
-						results_soft[image][neuron]);
-				printf("hard: image n°%3d, neurone n°%3d : %10lu\n",
-						image, neuron,
-						(unsigned long)
-						results_hard[image][neuron]);
-			}
-		}
-	}
-	if (!difference_soft_hard) {
-		printf("no difference between software and hardware results.\n");
-	}
 
 	/*
 	 * Classify the results.
 	 */
 	classify(&results_hard[0][0], classification_hard);
-	classify(&results_soft[0][0], classification_soft);
 
-#ifdef MNIST
 	/*
-	 * Compute hardware and software succes rates.
+	 * Compute hardware succes rates.
 	 */
 	for (image = 0; image < FRAMES_NB; image++) {
 		display(classification_hard[image]);
 		if (classification_hard[image] == labels[image]) {
 			success_hits_hard++;
 		}
-		if (classification_soft[image] == labels[image]) {
-			success_hits_soft++;
-		}
-
+		sleep(2);
 	}
+
 	printf("hardware success rate: %.2f %%\n",
 			(success_hits_hard / (float)FRAMES_NB) * 100);
-	printf("software success rate: %.2f %%\n",
-			(success_hits_soft / (float)FRAMES_NB) * 100);
-#endif
-
-	/*
-	 * Compare hard and soft timing performances.
-	 */
-	printf("%u frames\n", FRAMES_NB);
-	printf("hardware: %4.2g seconds => %5d frames/s\n",
-			hard_time, (int)(FRAMES_NB/hard_time));
-	printf("software: %4.2g seconds => %5d frames/s\n",
-			soft_time, (int)(FRAMES_NB/soft_time));
-	printf("speedup is %d\n", (int)(soft_time / hard_time));
 
 	while(1);
 
